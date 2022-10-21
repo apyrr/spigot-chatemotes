06032292ff1feaa45acdebeb166624faa079a0c3
apyrr <apyrr@users.noreply.github.com>
2022-10-22 01:11:32 +0300

    added frankerfacez to url handlers

--- src/main/kotlin/me/apyr/chatemotes/emote/local/EmoteResolver.kt
+++ src/main/kotlin/me/apyr/chatemotes/emote/local/EmoteResolver.kt
@@ -15,8 +15,12 @@ object EmoteResolver {
   private val urlHandlers: Map<Regex, (List<String>) -> String> = mapOf(
     Regex("""^(https://cdn.7tv.app/emote/\w+/)""") to { it[1] + "2x.png" },
     Regex("""^https://7tv.app/emotes/(\w+)""") to { "https://cdn.7tv.app/emote/${it[1]}/2x.png" },
-    Regex("""^(https://cdn.betterttv.net/emote/\w+/)""") to { it[1] + "2x" },
-    Regex("""^https://betterttv.com/emotes/(\w+)""") to { "https://cdn.betterttv.net/emote/${it[1]}/2x" }
+
+    Regex("""^(https://cdn.betterttv.net/emote/\w+)""") to { it[1] + "2x" },
+    Regex("""^https://betterttv.com/emotes/(\w+)""") to { "https://cdn.betterttv.net/emote/${it[1]}/2x" },
+
+    Regex("""^(https://cdn.frankerfacez.com/emoticon/\w+/)""") to { it[1] + "2" },
+    Regex("""^https://www.frankerfacez.com/emoticon/(\d+)""") to { "https://cdn.frankerfacez.com/emoticon/${it[1]}/2" },
   )
 
   private val pngMagic: ByteArray = byteArrayOf(
